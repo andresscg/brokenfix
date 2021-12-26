f4f100ea8c84acd28aa62d54acba9c4ba7e7b6f9
ernestoschubert <ernestoschubert23@gmail.com>
2021-12-26 17:33:32 -0300

    feat: agrega navbar sin menu hamburguesa

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -5,19 +5,26 @@ import { Link } from 'react-router-dom'
 const Navbar = () => {
   return (
     <div className="nav-container">
-      <img src="./assets/logo.svg" alt="logo" className="nav-logo" />
+      <Link to='/'>
+          <img src="./assets/logo.svg" alt="logo" className="nav-logo" />
+      </Link>
       <div className="nav-menu">
-        <Link to="/">
-          Home
-        </Link>
-        <Link to="/services">
-          Services
-        </Link>
-        <Link to="/login">
-          Log In
-        </Link>
-        <Link to="register">
+          <Link to="/" className='navbar-links'>
+            Home
+          </Link>
+          <Link to="/services" className='navbar-links'>
+            Services
+          </Link>
+          <Link to='/contact' className='navbar-links'>
+            Contact Us
+          </Link>
+      </div>
+      <div className='cont-log-sign'>
+        <Link to="register" className='navbar-links'>
           Sign Up
+        </Link>
+        <Link to="/login" className='navbar-links'>
+          Log In
         </Link>
       </div>
     </div>
