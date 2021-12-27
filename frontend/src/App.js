86fce0db682b0616f979d8b50ddeb348ce6878aa
andresscg <afelipecastillog@gmail.com>
2021-12-27 07:38:25 -0500

    Agregando algunas cosas

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,8 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Navbar from './components/Navbar';
 import Home from './pages/Home'
 import Sign from './pages/Sign';
+import Services from './pages/Services'
+import Service from './pages/Service'
 
 
 function App() {
@@ -11,6 +13,8 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/services/:id" element={<Service />} />
         <Route path="/sign" element={<Sign />} />
       </Routes>
     </BrowserRouter>
