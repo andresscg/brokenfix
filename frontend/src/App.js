6d6d9e0d604399c1467ff9330bd2f60ecfcec241
MarTakaes <maaritakaes@gmail.com>
2021-12-26 21:00:08 -0300

    Upload: hero and hero style´s

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import {BrowserRouter, Routes, Router} from 'react-router-dom'
 import Navbar from './components/Navbar'
+import HeroHome from './components/Hero';
 
 
 function App() {
   return (
     <BrowserRouter>
       <Navbar />
+      <HeroHome/>
     </BrowserRouter>
   );
 }
