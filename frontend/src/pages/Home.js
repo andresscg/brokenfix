b825d8b025180a9095446639e687b81874269b87
sulamittakauffman <sulamitta.kauffman@hotmail.com.br>
2021-12-27 13:31:58 -0300

    home en construccion

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -9,8 +9,16 @@ const Home = () => {
     <>
       <Hero />
       <body>
-        <main className="home-main d-flex justify-content-center p-5">
-          <h1>Home Services Broke&Fix</h1>
+        <main className="home-main">
+          <div className="d-flex justify-content-center p-5">
+            <h1>Home Services Broke&Fix</h1>
+          </div>
+          <div className="d-flex justify-content-center">
+            <img src="https://www.dealsshutter.com/blog/wp-content/uploads/2020/03/Home-Services-Image-30-march-2020.png"></img>
+          </div>
+          <div className="home-main-comments d-flex justify-content-end pe-4">
+            <h3 className="bg-light p-5">Comments Comments Comments</h3>
+          </div>
         </main>
       </body>
       <Footer />
