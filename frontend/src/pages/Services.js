d8e9c485ca73e0f1b008b026bd999f155f75737a
sulamittakauffman <sulamitta.kauffman@hotmail.com.br>
2021-12-28 00:53:28 -0300

    arranque services

--- frontend/src/pages/Services.js
+++ frontend/src/pages/Services.js
@@ -1,11 +1,55 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import servicesAction from "../redux/actions/servicesAction";
+import Loader from "../components/Loader";
+import "../styles/Services.css";
 
-const Services = () => {
+const Services = (props) => {
+  // FALTA EL FECHEO
+
+  const profesiones = this.props;
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <>
+      <div className="city-container">
+        <div className="subtitle-container">
+          <h2>FIND YOUR HOME SERVICE:</h2>
+        </div>
+        <div className="city-container-card">
+          {profesiones.length === 0 ? (
+            <Loader />
+          ) : profesiones.length > 0 ? (
+            profesiones.map((element) => {
+              return (
+                <div className="container-card" key={element._id}>
+                  <div key={element._id} className="city-img-container">
+                    <Link to="#">
+                      <img src={element.img} alt={element.name} />
+                    </Link>
+                    <div className="name-container">
+                      <p className="name-city">{element.name}</p>
+                    </div>
+                  </div>
+                </div>
+              );
+            })
+          ) : (
+            <p>Your search does not exist</p>
+          )}
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Services
+const mapStateToProps = (state) => {
+  return {
+    profesiones: state.services.listaProfesiones,
+  };
+};
+
+const mapDispatchToProps = {
+  buscarProfesion: servicesAction.buscarTodasProfesiones,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Services);
--- frontend/src/redux/reducers/servicesReducer.js
+++ frontend/src/redux/reducers/servicesReducer.js
@@ -1,5 +1,20 @@
-const citiesReducer = (state = {}, action) => {
+const inicialState = { listaProfesiones: [] };
 
-}
+const servicesReducer = (state = inicialState, action) => {
+  switch (action.type) {
+    case "BUSCAR_TODAS_PROFESIONES":
+      return {
+        ...state,
+        listaProfesiones: action.payload,
+      };
+    case "BUSCAR_PROFESION":
+      return {
+        ...state,
+        profesion: action.payload,
+      };
+    default:
+      return state;
+  }
+};
 
-export default citiesReducer;
+export default servicesReducer;
