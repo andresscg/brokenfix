28969a4a8b14c2d5e8b9f764b033969cc67a8ee3
MarTakaes <maaritakaes@gmail.com>
2021-12-29 13:54:25 -0300

    Feat:home changes

--- frontend/src/components/Homebody.js
+++ frontend/src/components/Homebody.js
@@ -21,22 +21,10 @@ const Homebody = () =>{
                             </footer>
                         </div>
                     </blockquote>
-         </div>
-         <div className="howto">
-                <h3 className="howtotitle">How to use our page</h3>
-                <div className="howtodiv1">
-                    <div className="innerdiv1">
-                    <p>1_ Sign In on the right-top of the page if you are looking for a professional <br></br>in one of the many areas we already have.</p>
-                    <p>2_ If you don't have an account yet, you can register for free by pressing the <br></br> sign up button.</p>
-                    <p>3_ Now you can complete the form o sign up using your Google account, its really <br></br> important to fill every space for the next step.</p>
-                    </div>
-                    <img className="howtosign" src="../assets/first.jpg"/>
                 </div>
-                <div>
-                    <p className="howtodiv2">Now that you are part of our community, you can look for the professional you need, we have a long list of workers from different areas that can help you with everything you need.
-Each worker has their own review, posted by clients from the page that includes a comment and a rating, we want to keep the page and community as well as now, so if you contact one of the workers please, do your own review to help the other customers and us by maintaining a safe environment. </p>
-                </div>
-         </div>
+             <div>
+               
+            </div>
         </>
     )
 }
