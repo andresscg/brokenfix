3571001e1d9c9af0df591f818d67404a2c080701
andresscg <afelipecastillog@gmail.com>
2021-12-28 23:23:04 -0500

    avanzando hasta donde pude por culpa de marcos :)

--- frontend/src/components/Review.js
+++ frontend/src/components/Review.js
@@ -1,6 +1,39 @@
 import React, {useState, useRef, useEffect} from 'react'
+import {useSelector} from 'react-redux';
+
 
 const Review = (props) => {
+
+  const userId = useSelector(state => state.users.user._id)
+  const token = useSlector(state => state.users.token)
+  const inputHandler = useRef()
+  const [shown, setShown] = useState(false)
+  const allowReview = props.newReview.user._id === userId
+
+  useEffect(() => {
+    setShown(false)
+  }, [props.render])
+
+  const reviewShowed = 
+    <div className="review-container">
+      {!shown 
+        ? 
+          <div className="review-data">
+            <p className="review-text">
+              {props.newReview.comment}
+            </p>
+            <p className="review-rating">
+              {props.newReview.rating}
+            </p>
+          </div>
+        :
+          <div className="edit-box">
+            <input type="text" defaultValue={props.newReview.comment} ref={inputHandler} />
+            <input type="number" defaultValue={props.newReview.rating} ref={inputHandler} />
+          </div>
+      }
+    </div>
+
   return (
     <div>
       
