4734ffb6133ded1cb76003be3001af8e5f234e7f
ernestoschubert <ernestoschubert23@gmail.com>
2021-12-30 05:56:22 -0300

    probando

--- server.js
+++ server.js
@@ -15,12 +15,12 @@ app.use(express.json());
 app.use(passport.initialize());
 app.use('/api', Router);
 
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static("client/build"));
-    app.get("*", (req, res) => {
-      res.sendFile(path.join(__dirname + "/client/build/index.html"));
-    });
-}
+// if (process.env.NODE_ENV === "production") {
+// app.use(express.static("client/build"));
+// app.get("*", (req, res) => {
+//       res.sendFile(path.join(__dirname + "/client/build/index.html"));
+//     });
+// }
 
 app.listen(process.env.PORT || "5000", () => {
     console.log(`El server esta en el puerto ${process.env.PORT || "5000"}`);
