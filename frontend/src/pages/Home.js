cce9193298360f70612fb4d8423161fd9d72b22c
ernestoschubert <ernestoschubert23@gmail.com>
2021-12-30 11:03:26 -0300

    agrega bodycard al home

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import Hero from '../components/Hero'
 import Homebody from '../components/Homebody';
+import BodyCard from '../components/BodyCard'
 
 const Home = () => {
     return(
      <>
         <Hero/> 
         <Homebody/>
-        
+        <BodyCard />
     </>
     )}
 
