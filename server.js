0cfa67e08f1c890c66179b7da9c3ef4b815f84dc
ernestoschubert <ernestoschubert23@gmail.com>
2021-12-30 05:46:48 -0300

    make it better

--- server.js
+++ server.js
@@ -6,10 +6,23 @@ const app = express();
 const passport = require('passport');
 require('./config/database');
 
+const path = require("path");
+var distDir = __dirname + "/dist/";
+app.use(express.static(distDir));
 
 app.use(cors());
 app.use(express.json());
 app.use(passport.initialize());
 app.use('/api', Router);
 
-app.listen(4000, () => console.log("Server running on port 4000"));
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+    app.get("*", (req, res) => {
+      res.sendFile(path.join(__dirname + "/client/build/index.html"));
+    });
+}
+
+app.listen(process.env.PORT || "5000", () => {
+    console.log(`El server esta en el puerto ${process.env.PORT || "5000"}`);
+  });
+  
